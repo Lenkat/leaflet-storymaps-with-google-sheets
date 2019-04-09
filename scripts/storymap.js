13e8e456c88cde2c63c9dab7fa5d7f205a8ccbcd
Lenkat <lenie095@gmail.com>
2019-04-09 14:55:48 +0200

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -117,7 +117,7 @@ $(window).on('load', function() {
             icon: L.ExtraMarkers.icon({
               icon: 'fa-number',
               number: ++chapterCount,
-              markerColor: 'blue'
+              markerColor: 'red'
             })
           }
         ));
